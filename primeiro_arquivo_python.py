e7cb5505c070883560f461d5256a55e18233fbc0
dev-DaviGuerra <davimguerra@gmail.com>
2025-08-04 13:06:39 -0300

    Boolean

--- primeiro_arquivo_python.py
+++ primeiro_arquivo_python.py
@@ -1,12 +1,16 @@
 print('-------INÍCIO-------')
 
-fome = input('Você está com fome? (Digite "s" para "sim" e "n" para "não"): ')
+estou_com_fome = input('Você está com fome? (Digite "s" para "sim" e "n" para "não"): ') == 's'
+tenho_comida = input('Tem comida em casa? (Digite "s" para "sim" e "n" para "não"): ') == 's'
 
-if fome == 's':
-    comida_em_casa = input('Você tem comida em casa? (Digite "s" para "sim" e "n" para "não"): ')
-    if comida_em_casa != 's':
-        print('Ir ao mercado')
-        print('Voltar para casa')
+if estou_com_fome and not tenho_comida:
+    print('Ir ao mercado')
+    print('Voltar para casa')
     print('Preparar uma refeição')
     print('Comer a comida')
-print('\n-------FIM-------')
+
+elif estou_com_fome and tenho_comida:
+    print('Preparar uma refeição')
+    print('Comer a comida')
+else:
+    print('\n-------FIM-------')
