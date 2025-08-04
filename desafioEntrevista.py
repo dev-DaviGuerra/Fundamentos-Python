d298686af90d347a1f0d641608d2724642ab4f4b
dev-DaviGuerra <davimguerra@gmail.com>
2025-08-04 12:34:42 -0300

    Condicionais

--- desafioEntrevista.py
+++ desafioEntrevista.py
@@ -1,7 +1,8 @@
-nome = str(input("Olá, digite o seu nome: "))
-idade = int(input("Agora digite a sua idade: "))
+nome = str(input(f"Olá, qual o seu nome\nDigite aqui: "))
+idade = int(input(f"Qual a sua idade?\nDigite aqui: "))
 quantidade_de_letras = len(nome)
 idade_futuro = idade + 5
 
-print(f"Olá {nome}, o seu nome tem {quantidade_de_letras} letras.")
-print(f"Daqui 5 anos você terá {idade_futuro} anos.")
+print('-' * 10)
+print(f"Olá {nome}, o seu nome tem {quantidade_de_letras} letras\nDaqui 5 anos você terá {idade_futuro} anos.")
+print('-' * 10)
--- primeiro_arquivo_python.py
+++ primeiro_arquivo_python.py
@@ -1,2 +1,8 @@
-digitar = input("Digite um número: ")
-print(f"O número digitado foi {digitar}")
+idade = int(input("Digite a sua idade: "))
+
+if idade < 18: 
+    print('Você tem menos de 18 anos.\nEntão você é menor de idade')
+elif idade == 18:
+    print('Você tem exatamente 18 anos.\nEntão você é maior de idade')
+else:
+    print('Você tem mais de 18 anos.\nEntão você é maior de idade')
