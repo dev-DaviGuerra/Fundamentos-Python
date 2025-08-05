5594d7df06c28977779788984f90c7e35c734110
dev-DaviGuerra <davimguerra@gmail.com>
2025-08-05 13:20:16 -0300

    Slicing

--- primeiro_arquivo_python.py
+++ primeiro_arquivo_python.py
@@ -1,33 +1,9 @@
+pessoas = ['Pedro', 'Thiago', 'João', 'Tomé']
+print(pessoas[1:3])
+print(pessoas[1:])
+print(pessoas[:])
+
 nome = 'Davi'
-print(nome[0])
-
-lista = [1, 2, 3]
-list(tuple(lista))
-print(lista)
-
-str(lista)
-print(lista)
-
-list(nome)
-print(nome)
-
-str()
-
-tuple()
-
-list()
-
-bool(10)
-
-bool(0)
-
-bool('')
-
-bool('asas')
-
-seq = [1, 2, 3]
-
-if seq: 
-    print('Sequência não está vazia')
-else:
-    print('Sequência está vazia')
+print(nome[1:])
+print(nome[:-1])
+print(nome[::-1])
