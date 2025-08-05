d71e8d094c13b8398d16d1c9ebabee2e4583bd97
dev-DaviGuerra <davimguerra@gmail.com>
2025-08-05 12:45:30 -0300

    Array em python

--- primeiro_arquivo_python.py
+++ primeiro_arquivo_python.py
@@ -1,38 +1,5 @@
-numero_certo = 31
-descobriu = False
+alunos = ['Davi', 'Fulano', 'Ciclano']
+print(alunos[-1])
 
-if not descobriu: 
-    chute = int(input('Digite o número: '))
-    if chute > numero_certo:
-        print('O número que você escolheu é maior que o número sorteado')
-    elif chute < numero_certo:
-        print('O número que você escolheu é menor que o número sorteado')
-    else:
-        print('Descobriu o número')
-        descobriu = True
-
-if not descobriu: 
-    chute = int(input('Digite o número: '))
-    if chute > numero_certo:
-        print('O número que você escolheu é maior que o número sorteado')
-    elif chute < numero_certo:
-        print('O número que você escolheu é menor que o número sorteado')
-    else:
-        print('Descobriu o número')
-        descobriu = True
-
-if not descobriu: 
-    chute = int(input('Digite o número: '))
-    if chute > numero_certo:
-        print('O número que você escolheu é maior que o número sorteado')
-    elif chute < numero_certo:
-        print('O número que você escolheu é menor que o número sorteado')
-    else:
-        print('Descobriu o número')
-        descobriu = True
-
-
-if descobriu:
-    print('Parabéns, você ganhou!')
-else:
-    print(f'Que pena, você perdeu! O número secreto era {numero_certo}')
+lista_misturada = [1, 'Python', [10, 4]]
+print(lista_misturada[-1][0])
