546ca600622b9b8cbb442e696bb83c906039e9e5
dev-DaviGuerra <davimguerra@gmail.com>
2025-08-08 13:42:27 -0300

    Metodos em python

--- primeiro_arquivo_python.py
+++ primeiro_arquivo_python.py
@@ -1,9 +1,32 @@
-while True:
-    opt = input('Escolha uma opção (1, 2) | "q" para sair: ')
-    if opt == 'q':
-        break
-    elif opt not in ('1', '2'):
-        print('Opção inválida! Digite 1 ou 2. ')
-        continue
-    else:
-        print(f'Opção selecionada: {opt}')
+s = 'Olá mundo!'
+
+num = 10
+
+dec = 3.33
+
+lista = [1, 2, 3]
+
+tupla = ('A', 'B', 'C')
+
+dic = {
+    'chave1': 'valor1',
+    'chave2': 'valor2',
+    'chave3': 'valor3'
+}
+
+produtos = {
+    'banana': 3.60,
+    'leite': 4.90,
+    'carne': 15.50,
+    'pão': 9.00
+}
+
+print(produtos)
+
+produtos.clear()
+
+print(produtos)
+
+print('-' *  100)
+
+print(dir(produtos))
