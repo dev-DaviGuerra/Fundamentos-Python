a2999c4cff47a99d3d748e1e84ff78b2596a519e
dev-DaviGuerra <davimguerra@gmail.com>
2025-08-13 17:00:30 -0300

    Compreensao de listas

--- primeiro_arquivo_python.py
+++ primeiro_arquivo_python.py
@@ -1,16 +1,16 @@
-texto = """
+valores = list(range(10))
 
-Python é uma linguagem de programação de alto nível,[10] interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte. Foi lançada por Guido van Rossum em 1991
+maiores_que_cinco = []
 
-A linguagem foi projetada com a filosofia de enfatizar a importância do esforço do programador sobre o esforço computacional
+for valor in valores:
+    if valor > 5:
+        maiores_que_cinco.append(valor)
 
-Python é uma linguagem de propósito geral de alto nível, multiparadigma, suporta o paradigma orientado a objetos, imperativo, funcional e procedural.
+resultado = [
+    valor + 10
+    for valor in valores 
+    if valor > 5
+]
 
-O nome Python teve a sua origem no grupo humorístico britânico Monty Python,[13] criador do programa Monty Python's Flying Circus, embora muitas pessoas façam associação com o réptil do mesmo nome (em português, píton ou pitão)
-
-"""
-vogais = ['a', 'e', 'i', 'o', 'u']
-
-for vogal in vogais:
-    contador_de_vogal = texto.lower().count(vogal)
-    print(f'Há {contador_de_vogal} vezes a vogal "{vogal}"')
+print(resultado)
+print(valores)
