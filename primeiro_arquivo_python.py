d8d59adea026356dddbb7be82d8081419802fb6c
dev-DaviGuerra <davimguerra@gmail.com>
2025-08-14 14:04:26 -0300

    Valor none

--- primeiro_arquivo_python.py
+++ primeiro_arquivo_python.py
@@ -1,16 +1,5 @@
-def adicionar_final(texto, final="!!!"):
-    return texto + final
-
-print(adicionar_final('Olá ', 'Mundo'))
-
-
-def dividir(n1, n2):
-    if n2 == 0:
-        return 'Impossível dividir'
-    return n1/n2
-    
-print(dividir(n1=10, n2=5))
-
-
 def dizer_ola():
     print('Olá!')
+
+retorno = dizer_ola()
+print(retorno)
